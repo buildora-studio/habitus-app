3ed245b980df8c2aca39f3614436ef46b54499dd
Brayan Sanjuan <bryan.sj175@gmail.com>
2026-02-06 18:14:02 -0500

    feat: update icon sizes for consistency and add AuthScreen component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,22 +3,24 @@ import { Tabs } from "expo-router";
 import React from "react";
 
 const ProfileIcon = ({ color }: { color: string }) => (
-  <Ionicons size={28} name="person-outline" color={color} />
+  <Ionicons size={24} name="person-outline" color={color} />
 );
 const HabitsIcon = ({ color }: { color: string }) => (
-  <Ionicons size={28} name="list-outline" color={color} />
+  <Ionicons size={24} name="list-outline" color={color} />
 );
 const DashboardIcon = ({ color }: { color: string }) => (
-  <Ionicons size={28} name="bar-chart-outline" color={color} />
+  <Ionicons size={24} name="bar-chart-outline" color={color} />
 );
 const GoalsIcon = ({ color }: { color: string }) => (
-  <Ionicons size={28} name="trophy-outline" color={color} />
+  <Ionicons size={24} name="trophy-outline" color={color} />
 );
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor: "gray",
         headerShown: false,
       }}
     >
